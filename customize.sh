f69fd55b373d2f64a7ed6c238be49d34f83551f5
Lokesh Vazirani <68844395+RADEON7@users.noreply.github.com>
2021-01-11 22:37:01 +0530

    Venom: Initial bringup.

---
+++ customize.sh
@@ -0,0 +1,28 @@
+##########################################################################################
+# MMMMM        MMMMM  MMMMMMMM  MMMMMMMM     MMMM  #
+#   MMMM     MMMM     MMMM      MMMM   MM    MMMM  #
+#     MMMM MMMM       MMMMMMMM  MMMM    MM   MMMM  #
+#        MMM          MMMM      MMMM     MM  MMMM  #   
+#         M           MMMMMMMM  MMMM      MMMMMMM  # Venom 2020
+##########################################################################################
+
+#Print welcome message
+ui_print "-Phone acting slow...i have solution.."
+ui_print "-Here is my venom to make ur phone into beast"
+ui_print "-Injecting Venom......."
+ui_print "-#################"
+ui_print "-1%"
+ui_print "-10%"
+ui_print "-20%"
+ui_print "-30%"
+ui_print "-40%"
+ui_print "-50%"
+ui_print "-60%"
+ui_print "-70%"
+ui_print "-80%"
+ui_print "-90%"
+ui_print "-99%"
+ui_print "-100%,Done"
+ui_print "-#################"
+ui_print "-Now ur phone has turned into symbiot"
+ui_print "-By Lokesh Vazirani, Telegram: @the_radeon"
